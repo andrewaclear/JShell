0223a1c13aa4e7623614dcf082987a268f868e42
suhchris <suhchris@UTORONTO.CA>
2020-10-26 01:08:12 +0000

    Added Execution, Parser and updated JShell with the main structure code

---
+++ Assignment2/src/driver/Execution.java
@@ -0,0 +1,14 @@
+package driver;
+
+public class Execution {
+  
+  /*Takes in an array of tokens and attempts to execute a command. 
+    Returns true if the command is sucessfully executed;
+    else returns false.
+  */
+  public boolean execute_command(String[] tokens) {
+    
+    
+    return true;
+  }
+}
---
+++ Assignment2/src/driver/Parser.java
@@ -0,0 +1,10 @@
+package driver;
+
+public class Parser {
+  //Takes in a line as a String and returns an array of tokens of type String
+  public String[] parse(String line) {
+    String[] temp = {"", ""};
+    return temp;
+  }
+  
+}
