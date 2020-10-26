958e3a5ac5cb66b79af4337a19438cd3a2b8dc9d
suhchris <suhchris@UTORONTO.CA>
2020-10-26 01:43:38 +0000

    Finished parse method in Parser

--- Assignment2/src/driver/Parser.java
+++ Assignment2/src/driver/Parser.java
@@ -3,8 +3,11 @@ package driver;
 public class Parser {
   //Takes in a line as a String and returns an array of tokens of type String
   public String[] parse(String line) {
-    String[] temp = {"", ""};
-    return temp;
+    String temp;
+    
+    temp = line.replaceAll(" +", " ");
+    temp = temp.trim();
+    
+    return temp.split(" ", 0);
   }
-  
 }
