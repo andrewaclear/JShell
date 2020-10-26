1a7310ed5221d9237a5536a659031b205f04b084
damario4 <damario4@UTORONTO.CA>
2020-10-26 21:05:37 +0000

    fixed package commands name and Exit close() function

--- Assignment2/src/commands/List.java
+++ Assignment2/src/commands/List.java
@@ -1,4 +1,4 @@
-package Commands;
+package commands;
 
 public class List extends Command {
 
@@ -6,10 +6,10 @@ public class List extends Command {
     String desc = "If no paths are given, print the contents"
         + " (file or directory) of the current directory, with a new line"
         + " following each of the content (file or directory).  Otherwise,"
-        + " for each path p, the order listed:  \n •If p specifies a file,"
-        + " print p \n •If p specifies a directory, print p, a colon, then the"
+        + " for each path p, the order listed:  \n ï¿½If p specifies a file,"
+        + " print p \n ï¿½If p specifies a directory, print p, a colon, then the"
         + " contents of that directory, then an extra new line. \n"
-        + " •If p does not exist, print a suitable message.  ";
+        + " ï¿½If p does not exist, print a suitable message.  ";
     
     String iden = "ls";
     this.setDescription(desc);
--- Assignment2/src/commands/MakeDirectory.java
+++ Assignment2/src/commands/MakeDirectory.java
@@ -1,4 +1,4 @@
-package Commands;
+package commands;
 
 public class MakeDirectory extends Command {
 
