eb16fe41c51dd714c0effb1732247a09ee2fb708
chenl147 <chenl147@UTORONTO.CA>
2020-10-27 21:48:49 +0000

    added the crc card of the node of the FileSystem and updated the crc card of FileSystem

--- Assignment2/src/data/FileSystem.java
+++ Assignment2/src/data/FileSystem.java
@@ -4,8 +4,14 @@ public class FileSystem {
   
  private FileSystemNode structure = new FileSystemNode(new Directory("Desktop"));
  
+ private FileSystemNode current_node = structure;
+ 
  public FileSystemNode getStructure() {
    return this.structure;
  }
-  
+ 
+ public FileSystemNode getCurrentDirectory() {
+   return this.current_node;
+ }
+ 
 }
