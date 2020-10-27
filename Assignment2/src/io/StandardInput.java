00a96ae67eb70b67e3d4927b45e24353d8f6d357
damario4 <damario4@UTORONTO.CA>
2020-10-27 21:51:12 +0000

    implemented StandardInput and improved JShell

--- Assignment2/src/io/StandardInput.java
+++ Assignment2/src/io/StandardInput.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 
 public class StandardInput {
 
-    public 
-        Scanner scan = new Scanner(System.in); // Counts as CommandLineReader
+    // Counts as CommandLineReader
+    private Scanner scan = new Scanner(System.in); 
+    public String current_line = "";
+    
+    public void nextLine() {
+        current_line = scan.nextLine(); //Awaits input 
+    }
 
+    public void close() {
+        scan.close();
+    }
 }
