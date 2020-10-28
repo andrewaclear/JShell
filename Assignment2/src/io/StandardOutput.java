9be6f7e947433ab88bb8f86c7e41adf0ac607688
damario4 <damario4@UTORONTO.CA>
2020-10-28 10:14:54 +0000

    added print functionality to StandardOutput and current dirctory name on terminal headline in JShell

--- Assignment2/src/io/StandardOutput.java
+++ Assignment2/src/io/StandardOutput.java
@@ -6,4 +6,8 @@ public class StandardOutput {
     public static void println(String message) {
         System.out.println(message);
     }
+
+    public static void print(String message) {
+        System.out.print(message);
+    }
 }
