32e075aa12d77d4ed8fd9d413275f1dcea0b3126
damario4 <damario4@UTORONTO.CA>
2020-10-28 12:57:09 +0000

    created dynamic structure for Command and its subclasses and implemented it into Exit, Errorhandler, and Execution

--- Assignment2/src/commands/Command.java
+++ Assignment2/src/commands/Command.java
@@ -3,6 +3,8 @@ package commands;
 public class Command {
   private String description;
   private String identifier;
+  private int numOfArguments;
+  private String errorTooManyArguments;
   
   public void setDescription(String desc) {
     this.description = desc;
@@ -11,4 +13,34 @@ public class Command {
   public void setIdentifier(String iden) {
     this.identifier = iden;
   }
+
+  public void setNumOfArguments(int num) {
+    this.numOfArguments = num;
+  }
+
+  public void setErrorTooManyArguments(String err) {
+    this.errorTooManyArguments = err;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public String getIdentifier() {
+    return this.identifier;
+  }
+
+  public int getNumOfArguments() {
+    return this.numOfArguments;
+  }
+
+  public String getErrorTooManyArguments() {
+    return this.errorTooManyArguments;
+  }
+
+
+  public boolean run(String[] tokens) {
+    return true;
+  }
+
 }
--- Assignment2/src/runtime/ErrorHandler.java
+++ Assignment2/src/runtime/ErrorHandler.java
@@ -1,15 +1,19 @@
 package runtime;
 
 import io.StandardOutput;
+import commands.Command;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-import commands.*;
+// import java.lang.reflect.Array;
+// import java.util.Arrays;
 
 public class ErrorHandler {
 
-    public static void Exit(String[] tokens) {
-        StandardOutput.println(Exit.badInput+Arrays.toString(Arrays.copyOfRange(tokens, 1, tokens.length)));
+    public static void tooManyArguments(Command command, String[] tokens) {
+        // StandardOutput.println(Exit.badInput+Arrays.toString(Arrays.copyOfRange(tokens, 1, tokens.length)));
+        StandardOutput.println(command.getIdentifier()+": "+command.getErrorTooManyArguments());
+    }
+
+    public static void commandNotFound(String[] tokens) {
+        StandardOutput.println(tokens[0]+": command not found");
     }
 }
