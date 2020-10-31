54928aed75a9568b0f84fe156923d3445d75fd84
chenl147 <chenl147@UTORONTO.CA>
2020-10-31 02:58:12 +0000

    Completed the code for ChangeDirectory and updated FileSystem to complement this command

--- Assignment2/src/commands/ChangeDirectory.java
+++ Assignment2/src/commands/ChangeDirectory.java
@@ -1,8 +1,27 @@
 package commands;
 
+import data.FileSystem;
+import data.FileSystemNode;
+
 public class ChangeDirectory extends Command {
   
-  public ChangeDirectory() {    
+  public ChangeDirectory() {
     this.setIdentifier("cd");
+    this.setMaxNumOfArguments(1);
+    this.setMinNumOfArguments(1);
   }
+  
+  public void doCommand(String path, FileSystem system) {
+    
+    //set note to the FileSystemNode that the path leads to if any
+    FileSystemNode node = system.getFileSystemNode(path);
+    
+    //set the FileSystem current_directory to node
+    if (node != null)
+    {
+      system.setCurrentDirectory(node);
+    }
+    
+  }
+  
 }
