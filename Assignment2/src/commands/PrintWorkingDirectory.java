b9eda7e302152ad0fe0d7e2280230630dbf7d792
chenl147@UTORONTO.CA <chenl147@UTORONTO.CA@2f7d7532-447b-4982-8c9f-17d765457761>
2020-10-31 02:59:48 +0000

    Finished the code for PrintWorkingDirectory

--- Assignment2/src/commands/PrintWorkingDirectory.java
+++ Assignment2/src/commands/PrintWorkingDirectory.java
@@ -1,8 +1,19 @@
 package commands;
 
+import data.FileSystem;
+
 public class PrintWorkingDirectory extends Command {
 
   public PrintWorkingDirectory() {
     this.setIdentifier("pwd");
+    this.setMaxNumOfArguments(0);
+    this.setMinNumOfArguments(0);
+  }
+  
+  public void doCommand(FileSystem system) {
+    
+    //print the path of the current directory
+    System.out.println(system.getCurrentDirectory().getPath());
+    
   }
 }
