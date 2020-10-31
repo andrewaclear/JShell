cc68565e92dbf6afee6069c9b13aff081d05c8d9
suhchris@UTORONTO.CA <suhchris@UTORONTO.CA@2f7d7532-447b-4982-8c9f-17d765457761>
2020-10-31 20:01:04 +0000

    Reworked Execution to make it more efficient using Reflection

--- Assignment2/src/runtime/Execution.java
+++ Assignment2/src/runtime/Execution.java
@@ -1,69 +1,65 @@
 package runtime;
 
 import data.FileSystem;
+import io.StandardOutput;
+import java.util.HashMap;
 import commands.*;
 
 public class Execution {
+  private HashMap<String, String> 
+    commandHashMap = new HashMap<String, String>();
   
   /*Takes in an array of tokens and attempts to execute a command. 
     Returns true if the command is successfully executed;
     else returns false. (in future returns an error msg)
   */
+  public Execution () {
+    initHashMap(commandHashMap);
+  }
+  
   public boolean execute_command(String[] tokens, FileSystem fSystem) {
     boolean run = true;
-    Command command = new Command();
     
-    switch(tokens[0]) {
-      case "cd":
-        //Execute the cd command here and set msg to an error if cd
-        //command runs into an error 
-        break;
-      case "cat":
-        command = new Concatenate();
-        break;
-      case "echo":
-        command = new Echo();
-        break;
-      case "exit":
-        command = new Exit();
-        break;
-      case "history":
-        command = new History();
-        break;
-      case "ls":
-        command = new ListContents(tokens.length);
-        break;
-      case "mkdir":
-        command = new MakeDirectory();
-        break;
-      case "man":
-        Manual manual = new Manual();
-        manual.printDescription(tokens);
-        break;
-      case "popd":
-        command = new PopDirectory();
-        break;
-      case "pushd":
-        command = new PushDirectory();
-        break;
-      case "pwd":
-        command = new PrintWorkingDirectory();
-        break;
-      default:
-        ErrorHandler.commandNotFound(tokens);
-        return run;
-        //Return command not found error
-    }
+    if (commandHashMap.containsKey(tokens[0])) {
+      String commandName = commandHashMap.get(tokens[0]);
+      
+      try {
+      Command command = (Command) Class.forName(commandName).
+          getDeclaredConstructor().newInstance();
+      
+      if (tokens.length <= command.getMaxNumOfArguments() && 
+          tokens.length >= command.getMinNumOfArguments()) {
+        run = command.run(tokens, fSystem);
+      } else if (tokens.length > command.getMaxNumOfArguments()) {
+        ErrorHandler.tooManyArguments(command);
+      } else if (tokens.length < command.getMinNumOfArguments()) {
+        ErrorHandler.missingOperand(command);
+      }
+      
+      } catch (Exception e) {
+        
+      }
 
-    if (tokens.length <= command.getMaxNumOfArguments() && 
-        tokens.length >= command.getMinNumOfArguments()) {
-      run = command.run(tokens, fSystem);
-    } else if (tokens.length > command.getMaxNumOfArguments()) {
-      ErrorHandler.tooManyArguments(command);
-    } else if (tokens.length < command.getMinNumOfArguments()) {
-      ErrorHandler.missingOperand(command);
+    } else {
+      ErrorHandler.commandNotFound(tokens);
     }
     
     return run;
   }
+  
+  private static void initHashMap(HashMap<String, String> commandHashMap)  {
+    commandHashMap.put("man", "commands.Manual");
+    commandHashMap.put("cd", "commands.ChangeDirectory");
+    commandHashMap.put("cat", "commands.Concatenate");
+    commandHashMap.put("echo", "commands.Echo");
+    commandHashMap.put("exit", "commands.Exit");
+    commandHashMap.put("history", "commands.History");
+    commandHashMap.put("ls", "commands.ListContents");
+    commandHashMap.put("mkdir", "commands.MakeDirectory");
+    commandHashMap.put("popd", "commands.PopDirectory");
+    commandHashMap.put("pwd", "commands.PrintWorkingDirectory");
+    commandHashMap.put("pushd", "commands.PushDirectory");
+  }
 }
+
+
