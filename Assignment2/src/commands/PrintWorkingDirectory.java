a2702def5d15e0e5e612af4666a7d6f2d76de797
chenl147@UTORONTO.CA <chenl147@UTORONTO.CA@2f7d7532-447b-4982-8c9f-17d765457761>
2020-11-04 03:50:13 +0000

    added error handling for MakeDirectory, ChangeDirectory and PrintWorkingDirectory

--- Assignment2/src/commands/PrintWorkingDirectory.java
+++ Assignment2/src/commands/PrintWorkingDirectory.java
@@ -8,10 +8,13 @@ public class PrintWorkingDirectory extends Command {
     this.setIdentifier("pwd");
     
     //PrintWorkingDirectory must not have any arguments
-    this.setMaxNumOfArguments(0);
-    this.setMinNumOfArguments(0);
+    this.setMaxNumOfArguments(1);
+    this.setMinNumOfArguments(1);
     this.setDescription("Print the current working directory"
         + " (including the whole path).  ");
+    this.setErrorTooManyArguments("no parameters are accepted");
+    this.setMissingOperand("identifier pwd is missing");
+    
   }
   
   //When run, PrintWorkingDirectory have to display the current Directory in the FileSystem
