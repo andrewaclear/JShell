8e5e4c4470f261cf3abd1e1a3162964478572e5b
suhchris@UTORONTO.CA <suhchris@UTORONTO.CA@2f7d7532-447b-4982-8c9f-17d765457761>
2020-11-07 22:53:41 +0000

    new cat

--- Assignment2/src/commands/Concatenate.java
+++ Assignment2/src/commands/Concatenate.java
@@ -63,6 +63,7 @@ public class Concatenate extends Command {
             }
           } else {
             ErrorHandler.fileNotFound(tokens[i]);
+            break;
           }
           i++;
       }
