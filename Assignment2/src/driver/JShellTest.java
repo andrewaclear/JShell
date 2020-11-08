88add59d97e03b222e2ee0c98ffebd46b002feb9
suhchris@UTORONTO.CA <suhchris@UTORONTO.CA@2f7d7532-447b-4982-8c9f-17d765457761>
2020-11-08 01:04:22 +0000

    Added JUnit testing

---
+++ Assignment2/src/driver/JShellTest.java
@@ -0,0 +1,15 @@
+package driver;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class JShellTest {
+
+  @Test
+  public void test() {
+    JShell shell = new JShell();
+    
+    //assertTrue()
+  }
+
+}
