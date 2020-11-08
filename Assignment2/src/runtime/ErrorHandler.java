7b50f05c8bd4d4d5cc6bd04e4f951efa169d4527
suhchris@UTORONTO.CA <suhchris@UTORONTO.CA@2f7d7532-447b-4982-8c9f-17d765457761>
2020-11-08 02:49:34 +0000

    Added javadocs

--- Assignment2/src/runtime/ErrorHandler.java
+++ Assignment2/src/runtime/ErrorHandler.java
@@ -5,46 +5,104 @@ import commands.Command;
 import data.FileSystem;
 import data.FileSystemNode;
 
-// import java.lang.reflect.Array;
-// import java.util.Arrays;
 
+/**
+ * Defines error messages for the various commands and prints the error
+ * messages to the terminal.
+ */
 public class ErrorHandler {
-
+    
+  /**
+   * Prints tooManyArguments error message
+   * 
+   * @param command, an instance of Command class or its subclasses
+   * @return void
+   */
     public static void tooManyArguments(Command command) {
         StandardOutput.println(command.getIdentifier()+": "
         +command.getErrorTooManyArguments());
     }
-
+    
+    /**
+     * Prints commandNotFound error message
+     * 
+     * @param tokens, array of string tokens holding command arguments
+     * @return void
+     */
     public static void commandNotFound(String[] tokens) {
         StandardOutput.println(tokens[0]+": command not found");
     }
     
+    /**
+     * Prints commandNotFoundManual error message
+     * 
+     * @param commandNotFound, name of command for which error occured
+     * @return void
+     */
     public static void commandNotFoundManual(String commandNotFound) {
       StandardOutput.println(commandNotFound + ": command not found");
     }
-
+    
+    /**
+     * Prints missingOperand error message
+     * 
+     * @param command, an instance of Command class or its subclasses
+     * @return void
+     */
     public static void missingOperand(Command command) {
         StandardOutput.println(command.getIdentifier()+": "
           +command.getErrorMissingOperand());
     }
     
+    /**
+     * Prints missingString error message
+     * 
+     * @param command, an instance of Command class or its subclasses
+     * @param tokens, array of string tokens holding command arguments
+     * @return void
+     */
     public static void missingString(Command command, String[] tokens) {
       StandardOutput.println(command.getIdentifier()+": "+tokens[1] 
         +": no string found, format string as \"string\"");
     }
     
+    /**
+     * Prints illegalString error message
+     * 
+     * @return void
+     */
     public static void illegalString() {
       StandardOutput.println("Illegal character in string");
     }
-
+    
+    /**
+     * Prints badInput error message
+     * 
+     * @param command, an instance of Command class or its subclasses
+     * @param message, error message to be printed
+     * @return void
+     */
     public static void badInput(Command command, String message) {
       StandardOutput.println(command.getIdentifier()+": "+message);
     }
     
+    /**
+     * Prints fileNotFound error message
+     * 
+     * @param file, name of file not found
+     * @return void
+     */
     public static void fileNotFound(String file) {
       StandardOutput.println(file + ": file not found");
     }
     
+    /**
+     * Prints invalidComboOfParams error message
+     * 
+     * @param command, an instance of Command class or its subclasses
+     * @param tokens, array of string tokens holding command arguments
+     * @return void
+     */
     public static void invalidComboOfParams(Command command, String[] tokens) {
       StandardOutput.print(command.getIdentifier() + ": ");
       for (int i = 1; i < tokens.length; i++) {
@@ -53,27 +111,60 @@ public class ErrorHandler {
       StandardOutput.print(": invalid combination of arguments\n");
     }
     
+    /**
+     * Prints invalidPath error message
+     * 
+     * @param command, an instance of Command class or its subclasses
+     * @return void
+     */
     public static void invalidPath(Command command) {
       StandardOutput.println(command.getIdentifier() +
           ": invalid path");
     }
     
+    /**
+     * Prints invalidName error message
+     * 
+     * @param command, an instance of Command class or its subclasses
+     * @param tokens, array of string tokens holding command arguments
+     * @return void
+     */
     public static void invalidName(Command command, String[] tokens) {
       StandardOutput.println(command.getIdentifier() +
           ": " + tokens[3] + ": invalid file and directory name");
     }
     
+    /**
+     * Prints childAlreadyExistant error message
+     * 
+     * @param directoryName, name of directory
+     * @param node, an instance of FileSystemNode that holds the position of
+     * child node in FileSystem
+     * @return void
+     */
     public static void childAlreadyExistant(String directoryName, 
         FileSystemNode node) {
       StandardOutput.println("The directory " + directoryName 
           + " already exists at " + node.getPath());
     }
     
+    /**
+     * Prints inappropriatePath error message
+     * 
+     * @param givenPath, the invalid path
+     * @return void
+     */
     public static void inappropriatePath(String givenPath) {
       StandardOutput.println("The given path : " + givenPath 
           + " constains illicit characters");
     }
     
+    /**
+     * Prints invalidPath error message
+     * 
+     * @param givenPath, the invalid path
+     * @return void
+     */
     public static void invalidPath(String givenPath) {
       StandardOutput.println("The given : " + givenPath 
           + " is not a valid path");
