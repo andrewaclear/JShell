207ebb3f64dbad3d9cb82c6a8d684498e7ebb19b
suhchris <suhchris@UTORONTO.CA>
2020-11-08 01:55:11 +0000

    fixed spelling

--- Assignment2/src/driver/JShell.java
+++ Assignment2/src/driver/JShell.java
@@ -26,12 +26,12 @@
 * The JShell program is a Java implementation of a bash shell. 
 * The following commands are included: exit, mkdir, cd, ls, pwd, pushd, popd,
 * history, cat, echo, man. 
-* The goal of this project is make smart design choices about when to use: 
+* The goal of this project is to make smart design choices about when to use: 
 * inheritance, composition, polymorphism, reflection and/or interfaces.
 * Further, it was to practice developing in an Agile environment; writing
 * dailyScrum meeting logs and meeting with our team to write sprint backlogs 
 * and a product backlog. Finally, we also created an auto tester using
-* JUnit and assert.
+* JUnit and assert and then generated documentation using javadocs.
 * Logistics: Collaborated and commited code using SVN
 * 
 * @author  Christopher Suh
