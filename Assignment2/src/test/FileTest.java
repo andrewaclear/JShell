948bca4f7d607f99b6d68f90c10e36ad7187b041
chenl147 <chenl147@UTORONTO.CA>
2020-11-10 18:12:48 +0000

    put the signature on the top of every of my classes

--- Assignment2/src/test/FileTest.java
+++ Assignment2/src/test/FileTest.java
@@ -21,6 +21,27 @@
 // help from no one in designing and debugging my program.
 // I have also read the plagiarism section in the course info
 // sheet of CSC B07 and understand the consequences.
+// Student1: Christian Chen Liu
+// UTORID user_name: Chenl147
+// UT Student #: 1006171009
+// Author: Christian Chen Liu
+//
+// Student2: Christopher Suh
+// UTORID user_name: suhchris
+// UT Student #: 1006003664
+// Author: Christopher Suh
+//
+// Student3: Andrew D'Amario
+// UTORID user_name: damario4
+// UT Student #: 1006618947
+// Author: Andrew D'Amario
+//
+//
+// Honor Code: I pledge that this program represents my own
+// program code and that I have coded on my own. I received
+// help from no one in designing and debugging my program.
+// I have also read the plagiarism section in the course info
+// sheet of CSC B07 and understand the consequences.
 // *********************************************************
 package test;
 
@@ -33,26 +54,39 @@ public class FileTest {
     //Create a new file with fileName "GoodFile"
     File myFile = new File("GoodFile");
     
-    //Print "The name of this file is GoodFile"
-    System.out.println("The name of this file is " + myFile.getFileName());
+    //The fileName of myFile should be GoodFile
+    if (!(myFile.getFileName().equals("GoodFile"))) {
+      System.out.println("ERROR: The name of this file is " 
+        + myFile.getFileName());
+    }
     
-    //Set the content of myFile to "This is some real content right inside here"
     myFile.setContent("This is some real content right inside here");
     
-    //Print "The content of this file is This is some real content right inside here"
-    System.out.println("The content of this file is " +  myFile.getContent());
-    
-    //Set the content of myFile to "The content is so so"
+    //The content of myFileName should be "This is some real content right 
+    //inside here"
+    if (!(myFile.getContent().equals("This is some real content right "
+        + "inside here"))) {
+      System.out.println("ERROR: The content of this file is " 
+        + myFile.getContent());
+    }
+
     myFile.setContent("This content is so so");
     
     //Print "The content of this file is This content is so so"
-    System.out.println("The content of this file is " +  myFile.getContent());
+    if (!(myFile.getContent().equals("This content is so so"))) {
+      System.out.println("ERROR: The content of this file is " 
+        +  myFile.getContent());
+    }
     
-  //Set the content of myFile to ""
     myFile.setContent("");
     
-    //Print "The content of this file is "
-    System.out.println("The content of this file is " +  myFile.getContent());
+    //The content of myFile should be ""
+    if (!(myFile.getContent().equals(""))) {
+      System.out.println("ERROR: The content of this file is " 
+       +  myFile.getContent());
+    }
+    
+    System.out.println("DONE");
     
   }
 
