e70d74b2261bc45783d41f56bd191cd2be3f4805
suhchris@UTORONTO.CA <suhchris@UTORONTO.CA@2f7d7532-447b-4982-8c9f-17d765457761>
2020-11-11 04:04:37 +0000

    Formatted

--- Assignment2/src/commands/MakeDirectory.java
+++ Assignment2/src/commands/MakeDirectory.java
@@ -31,30 +31,30 @@ import data.FileSystemNode;
 
 /**
  * This command takes in two arguments only. Create directories, each of which
- * may be relative to the current directory or may be a full path. If creating 
- * DIR1 results in any kind of error, do not proceed with creating DIR 2. 
- * However, if DIR1 was created successfully, and DIR2 creation results in an 
+ * may be relative to the current directory or may be a full path. If creating
+ * DIR1 results in any kind of error, do not proceed with creating DIR 2.
+ * However, if DIR1 was created successfully, and DIR2 creation results in an
  * error, then give back an error specific to DIR2.
  */
 public class MakeDirectory extends Command {
-  
+
   /**
-   * Constructor for MakeDirectory class. It initializes identifier, 
-   * maxNumOfArguments, minNumOfArguments errorTooManyArguments, 
-   * missingOperand, and description from its super class Commands.
+   * Constructor for MakeDirectory class. It initializes identifier,
+   * maxNumOfArguments, minNumOfArguments errorTooManyArguments, missingOperand,
+   * and description from its super class Commands.
    */
   public MakeDirectory() {
     this.setIdentifier("mkdir");
-    
-    //MakeDirectory must have three tokens
+
+    // MakeDirectory must have three tokens
     this.setMaxNumOfArguments(3);
     this.setMinNumOfArguments(3);
-    
-    //Error Handling
+
+    // Error Handling
     this.setErrorTooManyArguments("only two parameters are accepted");
     this.setMissingOperand("only two parameters are accepted");
-    
-    //Description
+
+    // Description
     this.setDescription("This command takes in two arguments only."
         + " Create directories, \neach of which may be relative"
         + " to the current directory or may \nbe a full path."
@@ -63,53 +63,52 @@ public class MakeDirectory extends Command {
         + " was created \nsuccessfully, and DIR2 creation results in an error,"
         + " then give \nback an error specific to DIR2. ");
   }
-  
+
   /**
    * The run method of MakeDirectory makes two directories in the given path
-   * tokens[1] and tokens[2] if both paths are valid/appropriate in 
-   * fileSystem, or makes a directory in the path tokens[1] if tokens[1] is a 
-   * valid/appropriate  path in fileSystem but tokens[2] is not, or 
-   * makes no directories at all if tokens[1] is not a valid/appropriate path 
-   * in fSystem. In any case, returns true after being done.
+   * tokens[1] and tokens[2] if both paths are valid/appropriate in fileSystem,
+   * or makes a directory in the path tokens[1] if tokens[1] is a
+   * valid/appropriate path in fileSystem but tokens[2] is not, or makes no
+   * directories at all if tokens[1] is not a valid/appropriate path in fSystem.
+   * In any case, returns true after being done.
    * 
    * @param tokens, array of string tokens holding command arguments
-   * @param fSystem, an instance of FileSystem class to read and write
-   * to the file structure.
+   * @param fSystem, an instance of FileSystem class to read and write to the
+   *        file structure.
    * @param cache, store the current directory stack
    * @return returns a boolean true to mark successful execution
    */
   @Override
   public boolean run(String[] tokens, FileSystem fSystem, Cache cache) {
-    
+
     FileSystemNode targetNode1 = null, targetNode2 = null;
 
     targetNode1 = fSystem.getSemiFileSystemNode(tokens[1]);
-    
-    targetNode2 = fSystem.getSemiFileSystemNode(tokens[2]);
-    
-    //Check if path1 is valid and the Directory already exists in the children 
-    //of FileSystemNode given by path1
-    if (targetNode1 != null && !targetNode1.isChildInside(
-        fSystem.getPathLastEntry(tokens[1]))) {
-        
-      //Add Directory to the FileSystemNode given by path1
-      targetNode1.addChild(new FileSystemNode(new Directory(
-          fSystem.getPathLastEntry(tokens[1]))));
-        
-        //Check if path2 is valid and if Directory already exists in the 
-        //of FileSystemNode
-        if (targetNode2 != null && !targetNode2.isChildInside(
-            fSystem.getPathLastEntry(tokens[2]))) {
-     
-           //Add Directory to the FileSystemNode given by path2
-           targetNode2.addChild(new FileSystemNode(new Directory(
-               fSystem.getPathLastEntry(tokens[2]))));
 
-        }
+    targetNode2 = fSystem.getSemiFileSystemNode(tokens[2]);
+
+    // Check if path1 is valid and the Directory already exists in the children
+    // of FileSystemNode given by path1
+    if (targetNode1 != null
+        && !targetNode1.isChildInside(fSystem.getPathLastEntry(tokens[1]))) {
+
+      // Add Directory to the FileSystemNode given by path1
+      targetNode1.addChild(new FileSystemNode(
+          new Directory(fSystem.getPathLastEntry(tokens[1]))));
+
+      // Check if path2 is valid and if Directory already exists in the
+      // of FileSystemNode
+      if (targetNode2 != null
+          && !targetNode2.isChildInside(fSystem.getPathLastEntry(tokens[2]))) {
+
+        // Add Directory to the FileSystemNode given by path2
+        targetNode2.addChild(new FileSystemNode(
+            new Directory(fSystem.getPathLastEntry(tokens[2]))));
+
+      }
     }
-   
+
     return true;
-    
+
   }
-  
 }
