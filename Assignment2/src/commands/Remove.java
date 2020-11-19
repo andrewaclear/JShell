bfe98c98fe5e85f918a8e291fae1fda7e257c2dc
chenl147 <chenl147@UTORONTO.CA>
2020-11-19 05:08:31 +0000

    the Tree command now also displays the files in a directory and remove no longer outputs an unexpected error message

--- Assignment2/src/commands/Remove.java
+++ Assignment2/src/commands/Remove.java
@@ -7,49 +7,68 @@ import runtime.ErrorHandler;
 
 public class Remove extends Command {
   
-    public Remove() {
-      this.setIdentifier("rm");
+  /**
+  * Constructor for Remove class. It initializes identifier,
+  * maxNumOfArguments, minNumOfArguments, errorTooManyArguments, 
+  * missingOperand, and description from its super class Commands.
+  */
+  public Remove() {
+    this.setIdentifier("rm");
   
-      // Remove must only have two tokens
-      this.setMaxNumOfArguments(2);
-      this.setMinNumOfArguments(2);
-  
-      // Error Handling
-      this.setErrorTooManyArguments("only one parameter is accepted");
-      this.setMissingOperand("only one parameter is accepted");
-  
-      // Description
-      this.setDescription("Removes the directory from the fileSystem "
-          + "including the children of the directory as well as the subpaths "
-          + "in the stack");
-  }
+    // Remove must only have two tokens
+    this.setMaxNumOfArguments(2);
+    this.setMinNumOfArguments(2);
     
-    public boolean run(String[] tokens, FileSystem fileSystem, Cache cache) {
+    // Error Handling
+    this.setErrorTooManyArguments("only one parameter is accepted");
+    this.setMissingOperand("only one parameter is accepted");
+    
+    // Description
+    this.setDescription("Removes the directory from the fileSystem "
+        + "including the children of the directory as well as the subpaths "
+        + "in the stack");
+  }
+  
+  
+  /**
+   * The run method of Remove removes the directory that the tokens[1] refers
+   * to and deletes all the paths in the stack that depended on tokens[1] 
+   * if tokens[1] is a valid/proper path, otherwise, it displays an error 
+   * message and return true.
+   * 
+   * @param tokens, array of string tokens holding command arguments
+   * @param fSystem, an instance of FileSystem class to read and write to the
+   *        file structure.
+   * @param cache, stores the history and directory stack of the running
+   *        terminal
+   * @return returns a boolean true signal the shell to continue running
+   */
+  public boolean run(String[] tokens, FileSystem fSystem, Cache cache) {
+    
+    FileSystemNode beforeNode = null;
+    
+    if (fSystem.getFileSystemNode(tokens[1]) != null) {
+      // Set targetNode to the FileSystemNode that the path leads to
+      beforeNode = fSystem.getSemiFileSystemNode(tokens[1]);
       
-      FileSystemNode beforeNode = null;
-      
-      if (fileSystem.getFileSystemNode(tokens[1]) != null) {
-        // Set targetNode to the FileSystemNode that the path leads to
-        beforeNode = fileSystem.getSemiFileSystemNode(tokens[1]);
-        
-        if (beforeNode != null) {
-          if (!fileSystem.getCurrentDirectory().getPath().startsWith(
-              fileSystem.getFileSystemNode(tokens[1]).getPath())) {
-            
-            beforeNode.removeChild(fileSystem.getPathLastEntry(tokens[1]));
-            
-            cache.removeDirectory(fileSystem.getFileSystemNode(
-                tokens[1]).getPath());
-            
-          } else {
-            
-            //ERROR: Cannot remove directories because it is a subdirectory 
-            ErrorHandler.removeDirectoryError(tokens[1]);
-          }
+      if (beforeNode != null) {
+        if (!fSystem.getCurrentDirectory().getPath().startsWith(
+            fSystem.getFileSystemNode(tokens[1]).getPath())) {
+          
+          cache.removeDirectory(fSystem.getFileSystemNode(
+              tokens[1]).getPath());
+          
+          beforeNode.removeChild(fSystem.getPathLastEntry(tokens[1]));
+          
+        } else {
+          
+          //ERROR: Cannot remove directories because it is a subdirectory 
+          ErrorHandler.removeDirectoryError(tokens[1]);
         }
-        
       }
-      return true;
+      
     }
+    return true;
+  }
     
 }
