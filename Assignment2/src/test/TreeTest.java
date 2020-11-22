57d6a8c594707668cc29da8a2bbe845878eb1744
chenl147 <chenl147@UTORONTO.CA>
2020-11-22 16:01:35 +0000

    started the tests for Remove and Tree

---
+++ Assignment2/src/test/TreeTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import data.FileSystem;
+
+public class TreeTest {
+  
+  void setUp() {
+    FileSystem myFileSystem = FileSystem.createFileSystem();
+  }
+  
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
