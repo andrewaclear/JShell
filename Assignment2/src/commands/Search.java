41bad1a253ac09b68345ad57b01db997ba860c5e
damario4@UTORONTO.CA <damario4@UTORONTO.CA@2f7d7532-447b-4982-8c9f-17d765457761>
2020-11-28 21:47:21 +0000

    completed Search class

--- Assignment2/src/commands/Search.java
+++ Assignment2/src/commands/Search.java
@@ -25,56 +25,80 @@ public class Search extends Command {
     this.setMissingOperand("Missing required arguments");
   }
 
+  private String searchForFiles (FileSystemNode node, String searchStr, String item, String path) {
+    if (node.getDirectory().getFiles() == null) return "";
+
+    int numFiles = node.getDirectory().getFiles().size();
+    String partOutput = "";
+    // boolean found = false;
+
+    for (int i = 0; i < numFiles; i++) {
+      if (node.getDirectory().getFiles().get(i).getFileName().equals(item)) {
+        partOutput += path + ": contains file: " + searchStr + "\n";
+        // found = true;
+        break;
+      }
+    }
+    // if (!found) partOutput += path + ": does not contain file: " + searchStr + "\n";
+
+    return partOutput;
+  }
+
+  private String searchNode(FileSystemNode node, String searchStr, String path, boolean fParam) {
+    if (node.getChildren() == null) return "";
+
+    String partOutput = "";
+    String subOutput = "";
+    String dirName = "";
+    String item = searchStr.replace("\"", "");
+    // boolean found = false;
+    int numFolders = node.getChildren().size();
+
+    for (int i = 0; i < numFolders; i++) {
+      dirName = node.getChildren().get(i).getDirectory().getDirectoryName();
+      if (!fParam && dirName.equals(item)) {
+        partOutput += path + ": contains directory: " + searchStr + "\n";
+        // found = true;
+      }
+      if (path.charAt(path.length()-1) != '/') path += "/";
+      subOutput += searchNode(node.getChildren().get(i), searchStr, path+dirName, fParam);
+    }
+    // if (!fParam && !found) {
+    //   partOutput += path + ": does not contain directory: " + searchStr + "\n";
+    // }
+
+    if (fParam) partOutput += searchForFiles(node, searchStr, item, path);
+
+    return partOutput + subOutput;
+  }
+  
   @Override
   public Command run(String[] tokens, JShell shell) {
     String output = "";
     FileSystemNode node;
-    boolean found;
     int len = tokens.length;
-    int n = 0;
-    String item = tokens[len - 1].replace("\"", "");
-    String name = tokens[len - 1];
+    String searchStr = tokens[len - 1];
+    String temp = "";
 
     boolean typeParam = tokens[len - 4].equals("-type");
     boolean fParam = tokens[len - 3].equals("f");
     boolean dParam = tokens[len - 3].equals("d");
     boolean nameParam = tokens[len - 2].equals("-name");
-    boolean stringParam = tokens[len - 1].charAt(0) == '\"';
+    boolean stringParam = searchStr.charAt(0) == '\"' && searchStr.charAt(searchStr.length()-1) == '\"';
 
     if (typeParam && (fParam || dParam) && nameParam && stringParam) {
       for (int iPath = 1; iPath < len - 4; iPath++) {
-        found = false;
         node = shell.getfSystem().getFileSystemNode(tokens[iPath]);
 
         if (node != null) {
-          if (fParam) {
-            n = node.getDirectory().getFiles().size();
-            for (int i = 0; i < n; i++) {
-              found = node.getDirectory().getFiles().get(i).getFileName()
-                  .equals(item);
-              if (found) {
-                output += tokens[iPath] + ": contains file: " + name + "\n";
-                break;
-              }
+          temp = searchNode(node, searchStr, tokens[iPath], fParam);
+          if (temp.equals("")) {
+            if (fParam) {
+              output += tokens[iPath] + ": does not contain file: " + searchStr + "\n";
+            } else {
+              output += tokens[iPath] + ": does not contain directory: " + searchStr + "\n";
             }
-            if (!found)
-              output +=
-                  tokens[iPath] + ": does not contain file: " + name + "\n";
-          } else {
-            n = node.getChildren().size();
-            for (int i = 0; i < n; i++) {
-              found = node.getChildren().get(i).getDirectory()
-                  .getDirectoryName().equals(item);
-              if (found) {
-                output +=
-                    tokens[iPath] + ": contains directory: " + name + "\n";
-                break;
-              }
-            }
-            if (!found)
-              output += tokens[iPath] + ": does not contain directory: " + name
-                  + "\n";
-          }
+          } else output += temp + "\n";
         } else {
           this.setErrors(ErrorHandler.invalidPath(this, tokens[iPath]));
           break;
