6e8ad4c173d9b784e619c7de4cc6f4eb18d71864
suhchris <suhchris@UTORONTO.CA>
2020-11-28 17:29:03 +0000

    Updated save and load JShell, curl to work with redirection

--- Assignment2/src/commands/LoadJShell.java
+++ Assignment2/src/commands/LoadJShell.java
@@ -3,6 +3,7 @@ package commands;
 import data.Cache;
 import data.FileSystem;
 import driver.JShell;
+import io.StandardOutput;
 import runtime.ErrorHandler;
 import java.io.*;
 
@@ -14,13 +15,13 @@ public class LoadJShell extends Command {
    * and description from its super class Commands.
    */
   public LoadJShell() {
-    this.setIdentifier("saveJShell");
+    this.setIdentifier("loadJShell");
     this.setDescription(
         "The file FileName is some file that is stored on the actual filesystem"
         + " of your computer. The purpose of this\r\n"
-        + "command is to save the session of the JShell before the user"
-        + " closes it down. ");
-    this.setMaxNumOfArguments(2);
+        + "command is to load the saved session of the JShell before the user"
+        + " closed it down. ");
+    this.setMaxNumOfArguments(4);
     this.setMinNumOfArguments(2);
     this.setErrorTooManyArguments("Too many arguments, please enter fileName");
     this.setMissingOperand("What file name, do wish to call the save?");
@@ -28,21 +29,27 @@ public class LoadJShell extends Command {
   
   @Override
   public Command run(String[] tokens, JShell shell) { 
-    if (shell.getCache().getHistorySize() == 0) {
-      try {
-        FileInputStream file = new FileInputStream(tokens[1]);
-        ObjectInputStream inStream = new ObjectInputStream(file);
-        shell.setfSystem((FileSystem) inStream.readObject());
-        shell.setCache((Cache) inStream.readObject());
-       
-        inStream.close();
-        file.close();
-        
-      } catch (IOException | ClassNotFoundException e) {
-        this.setErrors(ErrorHandler.invalidPath(this, tokens[1]));
+    boolean arrow = StandardOutput.containsArrow(tokens);
+    
+    if (!arrow && tokens.length == 2 || arrow && tokens.length == 4) {
+      if (shell.getCache().getHistorySize() == 1) {
+        try {
+          FileInputStream file = new FileInputStream(tokens[1]);
+          ObjectInputStream inStream = new ObjectInputStream(file);
+          shell.setfSystem((FileSystem) inStream.readObject());
+          shell.setCache((Cache) inStream.readObject());
+         
+          inStream.close();
+          file.close();
+          
+        } catch (IOException | ClassNotFoundException e) {
+          this.setErrors(ErrorHandler.invalidPath(this, tokens[1]));
+        }
+      } else {
+        this.setErrors(ErrorHandler.badInput(this, "Non-empty JShell"));
       }
     } else {
-      this.setErrors(ErrorHandler.badInput(this, "Non-empty JShell"));
+      this.setErrors(ErrorHandler.invalidComboOfParams(this, tokens));
     }
     return this;
   }
