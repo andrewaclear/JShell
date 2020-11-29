2ff2ca25b53222a84b0a920a218c119a239cde4a
suhchris <suhchris@UTORONTO.CA>
2020-11-29 02:55:10 +0000

    Completed echoTest

---
+++ Assignment2/src/test/EchoTest.java
@@ -0,0 +1,60 @@
+package test;
+
+import static org.junit.Assert.*;
+import java.lang.reflect.Field;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import commands.Concatenate;
+import commands.Echo;
+import data.FileSystem;
+import driver.JShell;
+
+public class EchoTest {
+
+  private Echo echo;
+  private JShell shell;
+
+  @Before
+  public void setUp() throws Exception {
+    shell = new JShell();
+    shell.setfSystem(FileSystem.createFileSystem());
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    Field field =
+        (shell.getfSystem().getClass()).getDeclaredField("fileSystem");
+    field.setAccessible(true);
+    field.set(null, null);
+  }
+  //Note: No need to handle invalid strings, since Parser takes care of them
+  // Correctly prints the given string and handles bad input
+  @Test
+  public void runTest1() throws Exception {
+    setUp();
+    echo = new Echo();
+
+    String[] actual = {"echo", "\"  Hello the world, test the world.  \""};
+    assertEquals("echo did not correctly print string",
+        "  Hello the world, test the world.  ",
+        echo.checkRun(actual, shell).getOutput());
+    String[] actual2 =
+        {"echo", "dssaasdsd", "\"  Hello the world, test the world.  \""};
+    assertEquals("echo did not correctly print string",
+        "echo: dssaasdsd \"  Hello the world, test the world.  \" : Invalid"
+        + " combination of arguments",
+        echo.checkRun(actual2, shell).getErrors());
+    String[] actual3 =
+      {"echo", "\"testering\"", "fd", "da"};
+  assertEquals("echo did not correctly print string",
+      "echo: \"testering\" fd da : Invalid"
+      + " combination of arguments",
+      echo.checkRun(actual3, shell).getErrors());
+
+
+    tearDown();
+  }
+
+}
