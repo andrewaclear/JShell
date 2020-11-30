5fe469d11c11b64e3d1a3ec6581ee3760125d1fc
suhchris <suhchris@UTORONTO.CA>
2020-11-30 21:12:20 +0000

    formatting

--- Assignment2/src/test/SearchTest.java
+++ Assignment2/src/test/SearchTest.java
@@ -47,7 +47,7 @@ public class SearchTest {
 
   private JShell shell;
   private Search search;
-  
+
   /**
    * @throws java.lang.Exception
    */
@@ -55,15 +55,16 @@ public class SearchTest {
   public void setUp() throws Exception {
     search = new Search();
     shell = new JShell();
-    shell.setfSystem(FileSystem.createFileSystem()); 
+    shell.setfSystem(FileSystem.createFileSystem());
   }
-  
+
   /**
    * @throws java.lang.Exception
    */
   @After
   public void tearDown() throws Exception {
-    Field field = (shell.getfSystem().getClass()).getDeclaredField("fileSystem");
+    Field field =
+        (shell.getfSystem().getClass()).getDeclaredField("fileSystem");
     field.setAccessible(true);
     field.set(null, null);
   }
@@ -80,7 +81,7 @@ public class SearchTest {
     redirect = new Redirection();
     String[] tokens3 = {"echo", "\"let's go\"", ">", "b/c"};
     redirect.run(tokens3, shell);
-    
+
     redirect = new Redirection();
     String[] tokens4 = {"echo", "\"oh ya\"", ">", "a/e/f/c"};
     redirect.run(tokens4, shell);
@@ -89,7 +90,8 @@ public class SearchTest {
   /**
    * some dirs, search for file, empty fSystem
    * 
-   * Test method for {@link commands.Search#run(java.lang.String[], driver.JShell)}.
+   * Test method for
+   * {@link commands.Search#run(java.lang.String[], driver.JShell)}.
    */
   @Test
   public final void testRun0() {
@@ -97,37 +99,42 @@ public class SearchTest {
     search.run(tokens, shell);
     assertEquals(".: does not contain file: \"hi\"", search.getOutput());
     assertEquals(null, search.getErrors());
-    
+
     String[] tokens2 = {"search", "l", "-type", "f", "-name", "\"hi\""};
     search = new Search();
     search.run(tokens2, shell);
     assertEquals(null, search.getOutput());
-    assertEquals("search: \"l\": No such file or directory", search.getErrors());
+    assertEquals("search: \"l\": No such file or directory",
+        search.getErrors());
   }
 
   /**
    * one dir, search for dir, empty fSystem
    * 
-   * Test method for {@link commands.Search#run(java.lang.String[], driver.JShell)}.
+   * Test method for
+   * {@link commands.Search#run(java.lang.String[], driver.JShell)}.
    */
   @Test
   public final void testRun1() {
     String[] tokens = {"search", ".", "-type", "d", "-name", "\"wowDir\""};
     search.run(tokens, shell);
-    assertEquals(".: does not contain directory: \"wowDir\"", search.getOutput());
+    assertEquals(".: does not contain directory: \"wowDir\"",
+        search.getOutput());
     assertEquals(null, search.getErrors());
 
     String[] tokens2 = {"search", "z", "-type", "d", "-name", "\"hi\""};
     search = new Search();
     search.run(tokens2, shell);
     assertEquals(null, search.getOutput());
-    assertEquals("search: \"z\": No such file or directory", search.getErrors());
+    assertEquals("search: \"z\": No such file or directory",
+        search.getErrors());
   }
 
   /**
    * one dir, search for file, non-empty fSystem
    * 
-   * Test method for {@link commands.Search#run(java.lang.String[], driver.JShell)}.
+   * Test method for
+   * {@link commands.Search#run(java.lang.String[], driver.JShell)}.
    */
   @Test
   public final void testRun2() {
@@ -153,14 +160,16 @@ public class SearchTest {
     search = new Search();
     search.run(tokens4, shell);
     assertEquals(null, search.getOutput());
-    assertEquals("search: \"/a/l\": No such file or directory", search.getErrors());
+    assertEquals("search: \"/a/l\": No such file or directory",
+        search.getErrors());
 
   }
 
   /**
    * one dir, search for directory, non-empty fSystem
    * 
-   * Test method for {@link commands.Search#run(java.lang.String[], driver.JShell)}.
+   * Test method for
+   * {@link commands.Search#run(java.lang.String[], driver.JShell)}.
    */
   @Test
   public final void testRun3() {
@@ -186,59 +195,75 @@ public class SearchTest {
     search = new Search();
     search.run(tokens4, shell);
     assertEquals(null, search.getOutput());
-    assertEquals("search: \"x\": No such file or directory", search.getErrors());
+    assertEquals("search: \"x\": No such file or directory",
+        search.getErrors());
 
   }
-  
+
   /**
    * some dirs, search for file, empty fSystem
    * 
-   * Test method for {@link commands.Search#run(java.lang.String[], driver.JShell)}.
+   * Test method for
+   * {@link commands.Search#run(java.lang.String[], driver.JShell)}.
    */
   @Test
   public final void testRun4() {
-    String[] tokens = {"search", "a", "b", "b/d", "-type", "f", "-name", "\"c\""};
+    String[] tokens =
+        {"search", "a", "b", "b/d", "-type", "f", "-name", "\"c\""};
     search.run(tokens, shell);
     assertEquals(null, search.getOutput());
-    assertEquals("search: \"a\": No such file or directory", search.getErrors());
+    assertEquals("search: \"a\": No such file or directory",
+        search.getErrors());
   }
 
   /**
    * some dirs, search for dir, empty fSystem
    * 
-   * Test method for {@link commands.Search#run(java.lang.String[], driver.JShell)}.
+   * Test method for
+   * {@link commands.Search#run(java.lang.String[], driver.JShell)}.
    */
   @Test
   public final void testRun5() {
     String[] tokens = {"search", "w", ".", "-type", "f", "-name", "\"wowDir\""};
     search.run(tokens, shell);
     assertEquals(null, search.getOutput());
-    assertEquals("search: \"w\": No such file or directory", search.getErrors());
+    assertEquals("search: \"w\": No such file or directory",
+        search.getErrors());
   }
 
   /**
    * some dirs, search for file, non-empty fSystem
    * 
-   * Test method for {@link commands.Search#run(java.lang.String[], driver.JShell)}.
+   * Test method for
+   * {@link commands.Search#run(java.lang.String[], driver.JShell)}.
    */
   @Test
   public final void testRun6() {
     fSystemSetup1();
-    String[] tokens = {"search", "a", "b", "b/d", "-type", "f", "-name", "\"c\""};
+    String[] tokens =
+        {"search", "a", "b", "b/d", "-type", "f", "-name", "\"c\""};
     search.run(tokens, shell);
-    assertEquals("a/e/f/c\nb/c\nb/d: does not contain file: \"c\"", search.getOutput());
+    assertEquals("a/e/f/c\nb/c\nb/d: does not contain file: \"c\"",
+        search.getOutput());
     assertEquals(null, search.getErrors());
 
-    String[] tokens2 = {"search", "a", "a", "a/e", "b/d", "b/a", "z", "a", "-type", "f", "-name", "\"c\""};
+    String[] tokens2 = {"search", "a", "a", "a/e", "b/d", "b/a", "z", "a",
+        "-type", "f", "-name", "\"c\""};
     search = new Search();
     search.run(tokens2, shell);
-    assertEquals("a/e/f/c\na/e/f/c\na/e/f/c\nb/d: does not contain file: \"c\"\nb/a: does not contain file: \"c\"", search.getOutput());
-    assertEquals("search: \"z\": No such file or directory", search.getErrors());
+    assertEquals(
+        "a/e/f/c\na/e/f/c\na/e/f/c\nb/d: does not contain file: \"c\"\nb/a:"
+            + " does not contain file: \"c\"",
+        search.getOutput());
+    assertEquals("search: \"z\": No such file or directory",
+        search.getErrors());
 
     String[] tokens3 = {"search", "a", "b/a", "-type", "f", "-name", "\"s\""};
     search = new Search();
     search.run(tokens3, shell);
-    assertEquals("a: does not contain file: \"s\"\nb/a: does not contain file: \"s\"", search.getOutput());
+    assertEquals(
+        "a: does not contain file: \"s\"\nb/a: does not contain file: \"s\"",
+        search.getOutput());
     assertEquals(null, search.getErrors());
 
   }
@@ -246,34 +271,50 @@ public class SearchTest {
   /**
    * some dirs, search for directory, non-empty fSystem
    * 
-   * Test method for {@link commands.Search#run(java.lang.String[], driver.JShell)}.
+   * Test method for
+   * {@link commands.Search#run(java.lang.String[], driver.JShell)}.
    */
   @Test
   public final void testRun7() {
     fSystemSetup1();
-    String[] tokens = {"search", "a", "b", "b/d", "-type", "d", "-name", "\"a\""};
+    String[] tokens =
+        {"search", "a", "b", "b/d", "-type", "d", "-name", "\"a\""};
     search.run(tokens, shell);
-    assertEquals("a: does not contain directory: \"a\"\nb/a\nb/d: does not contain directory: \"a\"", search.getOutput());
+    assertEquals(
+        "a: does not contain directory: \"a\"\nb/a\nb/d: does not contain"
+            + " directory: \"a\"",
+        search.getOutput());
     assertEquals(null, search.getErrors());
 
-    String[] tokens2 = {"search", "a", "a", "a/e", "b/d", "b/a", "x", "a", "-type", "d", "-name", "\"a\""};
+    String[] tokens2 = {"search", "a", "a", "a/e", "b/d", "b/a", "x", "a",
+        "-type", "d", "-name", "\"a\""};
     search = new Search();
     search.run(tokens2, shell);
-    assertEquals("a: does not contain directory: \"a\"\na: does not contain directory: \"a\"\na/e: does not contain directory: \"a\"\nb/d: does not contain directory: \"a\"\nb/a: does not contain directory: \"a\"", search.getOutput());
-    assertEquals("search: \"x\": No such file or directory", search.getErrors());
+    assertEquals(
+        "a: does not contain directory: \"a\"\na: does not contain directory:"
+            + " \"a\"\na/e: does not contain directory: \"a\"\nb/d: does not "
+            + "contain directory: \"a\"\nb/a: does not contain directory:"
+            + " \"a\"",
+        search.getOutput());
+    assertEquals("search: \"x\": No such file or directory",
+        search.getErrors());
 
     String[] tokens3 = {"search", "a", "b/a", "-type", "d", "-name", "\"s\""};
     search = new Search();
     search.run(tokens3, shell);
-    assertEquals("a: does not contain directory: \"s\"\nb/a: does not contain directory: \"s\"", search.getOutput());
+    assertEquals(
+        "a: does not contain directory: \"s\"\nb/a: does not contain directory:"
+            + " \"s\"",
+        search.getOutput());
     assertEquals(null, search.getErrors());
 
   }
-  
+
   /**
    * bad input
    * 
-   * Test method for {@link commands.Search#run(java.lang.String[], driver.JShell)}.
+   * Test method for
+   * {@link commands.Search#run(java.lang.String[], driver.JShell)}.
    */
   @Test
   public final void testRun8() {
@@ -281,19 +322,25 @@ public class SearchTest {
     String[] tokens = {"search", "a", "-name", "\"a\"", "-type", "d"};
     search.run(tokens, shell);
     assertEquals(null, search.getOutput());
-    assertEquals("search: a -name \"a\" -type d : Invalid combination of arguments", search.getErrors());
+    assertEquals(
+        "search: a -name \"a\" -type d : Invalid combination of arguments",
+        search.getErrors());
 
     String[] tokens2 = {"search", "hi", "type", "d", "name", "\"a\""};
     search = new Search();
     search.run(tokens2, shell);
     assertEquals(null, search.getOutput());
-    assertEquals("search: hi type d name \"a\" : Invalid combination of arguments", search.getErrors());
+    assertEquals(
+        "search: hi type d name \"a\" : Invalid combination of arguments",
+        search.getErrors());
 
     String[] tokens3 = {"search", "a", "b/a", "-", "f", "-S", "\"s\""};
     search = new Search();
     search.run(tokens3, shell);
     assertEquals(null, search.getOutput());
-    assertEquals("search: a b/a - f -S \"s\" : Invalid combination of arguments", search.getErrors());
+    assertEquals(
+        "search: a b/a - f -S \"s\" : Invalid combination of arguments",
+        search.getErrors());
 
   }
 
