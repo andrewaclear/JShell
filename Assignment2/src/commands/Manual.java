93ef5d72e8a767d2643f8478724d9862115ee463
suhchris@UTORONTO.CA <suhchris@UTORONTO.CA@2f7d7532-447b-4982-8c9f-17d765457761>
2020-11-30 23:11:33 +0000

    docstring

--- Assignment2/src/commands/Manual.java
+++ Assignment2/src/commands/Manual.java
@@ -85,10 +85,8 @@ public class Manual extends Command {
    * Prints out documentation for a specified command from tokens
    * 
    * @param tokens, array of string tokens holding command arguments
-   * @param fSystem, an instance of FileSystem class to read and write to the
-   *        file structure.
-   * @param cache, store the current directory stack
-   * @return returns a boolean true to mark successful execution
+   * @param shell, an instance of JShell
+   * @return returns an instance of the command
    */
   @Override
   public Command run(String[] tokens, JShell shell) {
